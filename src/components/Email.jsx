e86a10f428d73aeb993f9c6c71a45c25360b24ae
Sergey Aldoukhov <saldoukhov@gmail.com>
2020-03-23 13:35:50 -0700

    adding apple meta

--- src/components/Email.jsx
+++ src/components/Email.jsx
@@ -12,6 +12,7 @@ export default function Email(props) {
       <head>
         <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="x-apple-disable-message-reformatting" />
         <title>{props.title}</title>
         {props.headCSS && <style type="text/css">{props.headCSS}</style>}
       </head>
