4b6557e289a2dd62a223df463013ad1c39facdbd
Sergey Aldoukhov <saldoukhov@gmail.com>
2020-09-29 15:48:56 -0700

    add class name to content table

--- src/components/Email.jsx
+++ src/components/Email.jsx
@@ -28,7 +28,7 @@ export default function Email(props) {
       >
         <Box width="100%" height="100%" bgcolor={props.bgcolor}>
           <Item align={props.align} valign={props.valign}>
-            <Box width={props.width} align="center" cellPadding={props.cellPadding} cellSpacing={props.cellSpacing} style={props.style}>
+            <Box className="content" width={props.width} align="center" cellPadding={props.cellPadding} cellSpacing={props.cellSpacing} style={props.style}>
               {props.children}
             </Box>
           </Item>
